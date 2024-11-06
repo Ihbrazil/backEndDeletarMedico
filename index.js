97ac6f58f6725f99fb531064279f5d2275187caf
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-05 21:14:28 -0300

    Atualização Deletar pelo id
    Atualizações no back-end para Deletar médicos pelo campo id da tabela médico

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import express from 'express';
 import cors from "cors";
 
-import { apagaMedicoPorNome } from './servico/cadastroMedico.js';
+import { apagaMedico } from './servico/cadastroMedico.js';
 
 const app = new express();
 
 app.use(cors());
 app.use(express.json());
 
-app.delete("/medicos/:nome", async (req, res) => {
-    const nome = req.params.nome;
-
+app.delete("/medicos/:id", async (req, res) => {
+    const idMedico = req.params.id;
     try {
-        const resultado = await apagaMedicoPorNome(nome);
-        if (resultado) {
-            res.status(204).end();
+        const resultado = await apagaMedico(idMedico);
+        if (resultado.affectedRows > 0) {
+            res.status(200).send({ mensagem: "Médico apagado com sucesso." });
         } else {
             res.status(404).send({ mensagem: "Médico não encontrado" });
         }
+
     } catch (erro) {
+        
         res.status(500).send({ mensagem: "Erro ao tentar apagar o médico", erro: erro.message });
+        
     }
 });
 
--- servico/cadastroMedico.js
+++ servico/cadastroMedico.js
@@ -1,17 +1,22 @@
 import pool from "./conexao.js";
 
-export async function apagaMedicoPorNome(nome) {
+export async function apagaMedico(idMedico) {
     const conexao = await pool.getConnection();
  
     try {
-        const resposta = await conexao.query("DELETE FROM medicos WHERE LOWER(nome) = ?", [nome.toLowerCase()]);
+        
+        const [resposta] = await conexao.query("DELETE FROM medicos WHERE id = ?", [idMedico]); // Faltavam os colchetes em resposta para retornar o objeto correto. Os colchetes são necessários para desestruturar o array retornado pela consulta e obter o objeto de resposta correto. resposta será um array com dois elementos. Para acessar o objeto que contém os resultados da consulta, você teria que fazer algo como resposta[0]. Aqui, estamos usando a desestruturação de arrays. [resposta] O que isso faz é pegar o primeiro elemento do array retornado pela consulta e atribuí-lo à variável resposta. Isso simplifica o acesso aos dados que realmente nos interessam.
 
-        conexao.release();
- 
         return resposta;
-
+        
     } catch (erro) {
-        conexao.release();
+        
+        console.error("Erro ao tentar apagar o médico:", erro);
         throw erro;
+
+    } finally {
+        
+        conexao.release();
+        
     }
  }
